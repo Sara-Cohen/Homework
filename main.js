cc2cfaad287344c5e2c9d210286afec04b2e3027
Sara Cohen <83020298+Sara-Cohen@users.noreply.github.com>
2021-08-03 18:02:27 +0300

    סופי

--- main.js
+++ main.js
@@ -1,75 +1,91 @@
 
-
-let className;
-
-const App =()=>{
-return (
-  <nav class="product-filter">
-  <h1>Jackets</h1>
-    <Header/>
-    <TheProducts/> 
-     </nav>);};
-
-
-const Header=()=> {
-return (
-  <div class="sort">
-    <div class="collection-sort">
-      <label>Filter by:</label>
-      <select>
-        <option value="/">All Jackets</option>
-        <option value="/">2016</option>
-        <option value="/">jacket</option>
-        <option value="/">Jackets</option>
-        <option value="/">layers</option>
-        <option value="/">Obeyer</option>
-        <option value="/">Roxy</option>
-        <option value="/">women's</option>
-      </select>
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Products />
     </div>
-    <div class="collection-sort">
-      <label>Sort by:</label>
-      <select>
-        <option value="/">Featured</option>
-        <option value="/">Best Selling</option>
-        <option value="/">Alphabetically, A-Z</option>
-        <option value="/">Alphabetically, Z-A</option>
-        <option value="/">Price, low to high</option>
-        <option value="/">Price, high to low</option>
-        <option value="/">Date, new to old</option>
-        <option value="/">Date, old to new</option>
-      </select>
-    </div>
-  </div>
-       );};
-
-const TheProducts = () => {
-return (
-  <section className="products">
-    The products:
-    <Product/><br/>
-    <Product/>
-    <Product/>
-    <br/>
-    <Product/>
-   <Product/>
-   <Product/>
-  </section>
-  
-);};
-
-const Product=()=>{
- return (
-    <div class="product-card">
-      <div class="product-image">
-        <img
-          src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
-        />
+  );
+};
+const Header = () => {
+  return (
+    < nav className="product-filter">
+      <h1>Jackets</h1>
+      <div className="sort">
+        <div className="collection-sort">
+          <label>Filter by:</label>
+          <select>
+            <option value="/">All Jackets</option>
+            <option value="/">2016</option>
+            <option value="/">jacket</option>
+            <option value="/">Jackets</option>
+            <option value="/">layers</option>
+            <option value="/">Obeyer</option>
+            <option value="/">Roxy</option>
+            <option value="/">women's</option>
+          </select>
+        </div>
+        <div className="collection-sort">
+          <label>Sort by:</label>
+          <select>
+            <option value="/">Featured</option>
+            <option value="/">Best Selling</option>
+            <option value="/">Alphabetically, A-Z</option>
+            <option value="/">Alphabetically, Z-A</option>
+            <option value="/">Price, low to high</option>
+            <option value="/">Price, high to low</option>
+            <option value="/">Date, new to old</option>
+            <option value="/">Date, old to new</option>
+          </select>
+        </div>
       </div>
-      <div class="product-info">
-        <h5>Winter Jacket</h5>
-        <h6>$99.99</h6>
+    </nav>
+  );
+};
+const Products = () => {
+  return (
+    <section className="products">
+      <Product
+        image="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
+        title="Winter Jacket"
+        price="$99.99" />
+      <Product
+        image="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
+        title="Winter Jacket"
+        price="$99.99" />
+      <Product
+        image="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
+        title="Winter Jacket"
+        price="$99.99" />
+      <Product
+        image="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
+        title="Winter Jacket"
+        price="$99.99" />
+      <Product
+        image="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
+        title="Winter Jacket"
+        price="$99.99" />
+      <Product
+        image="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
+        title="Winter Jacket"
+        price="$99.99" />
+    </section>
+  );
+};
+const Product = ({ image, title, price }) => {
+  return (
+    <div className="product-card">
+      <div className="product-image">
+        <img src={image} />
+      </div>
+      <div className="product-info">
+        <h5>{title}</h5>
+        <h6>{price}</h6>
       </div>
     </div>
- );};
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"))
+
+
+
